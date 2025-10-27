833fb62283ad0c4fa67d629a84914498dc4de3f0
Lucas Funchal da Silva <CIANDT\lucasfunchal@lnb028827cps.ciandt.global>
2025-10-27 11:15:43 -0300

    removendo use case não utilizado

--- src/main/java/br/com/outsera/worse_movie/application/port/SaveWorseMoviesUseCase.java
+++
@@ -1,9 +0,0 @@
-package br.com.outsera.worse_movie.application.port;
-
-import br.com.outsera.worse_movie.domain.movie.Movie;
-
-import java.util.List;
-
-public interface SaveWorseMoviesUseCase {
-    void execute(List<Movie> request);
-}
